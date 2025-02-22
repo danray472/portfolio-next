458b173bba892c666edf35ea9e532017acbf48a7
collo-ngei <ccheruiyot868@gmail.com>
2025-02-22 05:45:24 +0300

    Fix next.config for Vercel deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
-    experimental: {
-      appDir: true, // Enables the App Router
-    },
     output: "standalone", // Required for Vercel deployment
   };
   
